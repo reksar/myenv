998b9df5fb0e383f63fe736c245e6fe45f9c3c6d
reksarka <reksarka@gmail.com>
2022-03-15 20:54:39 +0200

    Sets the python alias to python3 if possible.

---
+++ linux/python-alias.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Sets the python alias to python3 if possible.
+
+major_version() {
+  $1 --version | grep -Eo '[0-9]{1,}' | head -1
+}
+
+if [ `which python` ] && [ `major_version python` == 3 ]
+then
+  echo The python alias is already set to python3
+  exit 0
+fi
+
+python3=`which python3`
+
+if ! [ $python3 ]
+then
+  echo The python3 is not found
+  exit 1
+fi
+
+update-alternatives --force --install /usr/bin/python python $python3 3
